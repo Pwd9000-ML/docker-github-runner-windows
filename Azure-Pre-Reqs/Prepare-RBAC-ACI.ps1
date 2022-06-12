37fd670f1f32bb68da8b684ba136b41d5837928e
M L <magiconion_M@hotmail.com>
2022-06-12 15:28:26 +0100

    test

--- Azure-Pre-Reqs/Prepare-RBAC-ACI.ps1
+++ Azure-Pre-Reqs/Prepare-RBAC-ACI.ps1
@@ -3,7 +3,7 @@
 
 # Setup Variables.
 $aciResourceGroupName = "Demo-ACI-GitHub-Runners-RG"
-$appName = "GitHub-ACI-Deploy" #Previously created Service Principal (See part 3 of blog series)
+$appName="GitHub-ACI-Deploy" #Previously created Service Principal (See part 3 of blog series)
 $region = "uksouth"
 
 # Create a resource group to deploy ACIs to
@@ -15,15 +15,4 @@ az ad sp list --display-name $appName --query [].appId -o tsv | ForEach-Object {
     az role assignment create --assignee "$_" `
         --role "Contributor" `
         --scope "$aciRGId"
-}az ad sp create-for-rbac --name $appName `
-    --role "Contributor" `
-    --scopes "$aciRGId" `
-    --sdk-auth
-
-# Assign additional RBAC role to Service Principal to push and pull images from ACR 
-$acrId = az acr show --name "$acrName" --query id --output tsv
-az ad sp list --display-name $appName --query [].appId -o tsv | ForEach-Object {
-    az role assignment create --assignee "$_" `
-        --role "AcrPush" `
-        --scope "$acrId"
-}
+    }
