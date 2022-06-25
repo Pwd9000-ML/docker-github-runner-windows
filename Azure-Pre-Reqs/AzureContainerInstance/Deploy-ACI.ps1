9ca71bc6acc8b0a74abddd75dd0522aa3f100d10
M L <magiconion_M@hotmail.com>
2022-06-25 14:20:12 +0100

    update repo structure

--- Azure-Pre-Reqs/AzureContainerInstance/Deploy-ACI.ps1
+++ Azure-Pre-Reqs/AzureContainerInstance/Deploy-ACI.ps1
@@ -4,12 +4,12 @@
 $randomInt = Get-Random -Maximum 9999
 $aciResourceGroupName = "Demo-ACI-GitHub-Runners-RG" #Resource group created to deploy ACIs
 $aciName = "gh-runner-windows-$randomInt" #ACI name (unique)
-$acrLoginServer = "registryName.azurecr.io" #The login server name of the ACR (all lowercase). Example: _myregistry.azurecr.io_
+$acrLoginServer = "registryname.azurecr.io" #The login server name of the ACR (all lowercase). Example: _myregistry.azurecr.io_
 $acrUsername = "servicePrincipalClientId" #The `clientId` from the JSON output from the service principal creation (See part 3 of blog series)
 $acrPassword = "servicePrincipalClientSecret" #The `clientSecret` from the JSON output from the service principal creation (See part 3 of blog series)
-$image = "$acrLoginServer/pwd9000-github-runner-win:2.293.0" #image reference to pull
+$image = "$acrLoginServer/pwd9000-github-runner-win:2.293.0" #Image reference to pull
 $pat = "githubPAT" #GitHub PAT token
-$githubOrg = "Pwd9000-ML" #GitHub Owner
+$githubOrg = "Pwd9000-ML" #GitHub Owner/Org
 $githubRepo = "docker-github-runner-windows" #GitHub repository to register self hosted runner against
 $osType = "Windows" #Use "Linux" if image is Linux OS
 
